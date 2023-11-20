e89bf759adbc728a0a868e81c5f43547d8f80ecb
manish <manishpanwar682@gmail.com>
2023-11-19 22:09:46 -1200

    hello

---
+++ server.js
@@ -0,0 +1,20 @@
+const  dotenv = require('dotenv');
+dotenv.config();
+require('./db.js')
+const  express = require('express');
+const userRoute = require("./router/user.js");
+const  cors = require('cors');
+const foodRouter = require('./router/food.js')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get('/api',(req,res)=>{
+  res.send('hello manish')
+})
+app.use('/api/foods', foodRouter);
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log('server started ');
+});
